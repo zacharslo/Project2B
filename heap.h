110ec527dbf2bfb9ad9860368d50eb2f0a9c7b4a
Andrew <gamefreak0321@gmail.com>
2015-02-12 15:49:36 -0500

    added includes and stuff
    getting error

--- heap.h
+++ heap.h
@@ -1,29 +1,38 @@
+//By Andrew Blum and Zach Sloane
 
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+#include <math.h>
 
+using namespace std;
 
 class heap {
 	public:
+		heap(const vector<string> wordlist);
 		string parent(const int index);
 		heap left(const int index);
 		heap right(const int index);
 		void getItem(const int n);
 		
 		void initializeMaxHeap();
-		void maxHeapify();
+		void maxHeapify(const int index);
 		void buildMaxHeap();
 		
 		void initializeMinHeap();
-		void minHeapify();
+		void minHeapify(const int index);
 		void buildMinHeap();
 		
 		void heapsort();
 		
-		void size();
+		int size;
 		
 	private:
-		string parent;
+		string p;
 		heap leftChild;
 		heap rightChild;
 		int index;
-		int size;
 };
